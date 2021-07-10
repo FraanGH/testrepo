f6adb3815a3025b3c665b5cd3b144b309ee0070b
Francisco Gonzalez <franciscogonza2002@gmail.com>
2021-07-10 17:29:33 -0300

    Modified firstpython.py with VSCode

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,4 @@
 # Mostrar output
 print("Es un archivo python")
 print("Esta linea de código fue añadida posteriormente desde Theia!")
+print("Y esta linea de código fue escrita desde Bash usando VSCODE!")
